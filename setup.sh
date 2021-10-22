49376bd0cb9b3dbf2b172b17fa23bf6b36e43f66
Kalabuk Dmitriy <kalabukdima@gmail.com>
2021-10-22 12:01:13 +0300

    Fix setup.sh

--- setup.sh
+++ setup.sh
@@ -1,3 +1,5 @@
+git submodule update --init --recursive
+
 ln -sfv ~/config/.bashrc ~
 touch ~/config/.bashrc.local
 
@@ -14,6 +16,10 @@ ln -sfv ~/config/ranger/ ~/.config/
 
 ln -sfv ~/config/openbox/ ~/.config/
 
-sudo -n mkdir -p /usr/share/kbd/keymaps/i386/workman
-sudo -n cp -v kbd/keymaps/i386/workman/* /usr/share/kbd/keymaps/i386/workman/
-sudo -n cp -v kbd/keymaps/i386/include/workman-layout.inc /usr/share/kbd/keymaps/i386/include
+chsh -s /usr/bin/zsh
+
+sudo ln -sfv ~/config/xorg.conf.d/* /etc/X11/xorg.conf.d/
+
+sudo mkdir -p /usr/share/kbd/keymaps/i386/workman
+sudo cp -v kbd/keymaps/i386/workman/* /usr/share/kbd/keymaps/i386/workman/
+sudo cp -v kbd/keymaps/i386/include/workman-layout.inc /usr/share/kbd/keymaps/i386/include
